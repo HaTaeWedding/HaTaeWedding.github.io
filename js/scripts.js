131ecc9c68680d4b2d54275ce8aa1c12564639d1
HaTaeWedding <rhxogns49@gmail.com>
2024-06-26 11:30:56 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -114,17 +114,11 @@ window.onclick = function(event) {
 }
 
 function shareToBand() {
-    const shareUrl = "https://HaTaeWedding.github.io"; // 공유할 URL
-    const shareText = "태훈&하영 결혼합니다";
-    const imageUrl = "https://hataeWedding.github.io/assets/img/portfolio/wedding/ifwe1.jpg"; // 공유할 이미지 URL
+    var shareUrl = "https://HaTaeWedding.github.io"; // 공유할 URL
+    var encodedShareUrl = encodeURIComponent(shareUrl);
+    var bandAppUrl = `bandapp://create/post?text=${encodedShareUrl}`;
 
-    const encodedShareUrl = encodeURIComponent(shareUrl);
-    const encodedShareText = encodeURIComponent(shareText);
-    const encodedImageUrl = encodeURIComponent(imageUrl);
-
-    const bandWebUrl = `https://band.us/plugin/share?body=${encodedShareText}%0A${encodedShareUrl}&route=${encodedShareUrl}&image=${encodedImageUrl}`;
-
-    window.location.href = bandWebUrl;
+    window.location.href = bandAppUrl;
 }
 
 
@@ -135,9 +129,9 @@ Kakao.Share.createDefaultButton({
     container: '#kakaotalk-sharing-btn',
     objectType: 'feed',
     content: {
-        title: '태훈&하영 결혼합니다.',
-        description: '10/12(토), 제주썬호텔 3층 대연회장',
-        imageUrl: imgUrl + '/assets/img/share11.jpg',
+        title: '태훈&하영 결혼합니다',
+        description: '10/12(토), 제주썬호텔 3층 Ballroom',
+        imageUrl: imgUrl + '/assets/img/main.jpg',
         link: {
             // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
             mobileWebUrl: 'https://hataewedding.github.io',
@@ -177,72 +171,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
     elements.forEach(element => {
         observer.observe(element);
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var portfolioSection = document.getElementById('portfolio');
-    var lastScale = 1;
-
-    function checkZoomLevel(event) {
-        var zoomLevel = window.visualViewport.scale;
-        if (zoomLevel > 1) {
-            portfolioSection.classList.add('zoomed');
-        } else {
-            portfolioSection.classList.remove('zoomed');
-        }
-    }
-
-    window.visualViewport.addEventListener('resize', checkZoomLevel);
-    window.addEventListener('scroll', checkZoomLevel);
-
-    // 초기 로딩 시에도 한 번 체크
-    checkZoomLevel();
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    // SimpleLightbox 초기화
-    var lightbox = new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
-    var portfolioSection = document.getElementById('portfolio');
-
-    function checkZoomLevel() {
-        var zoomLevel = window.visualViewport.scale;
-        var modalImage = document.querySelector('.simple-lightbox img');
-
-        if (zoomLevel > 1) {
-            portfolioSection.classList.add('zoomed');
-            if (modalImage) {
-                modalImage.classList.add('zoomed');
-            }
-        } else {
-            portfolioSection.classList.remove('zoomed');
-            if (modalImage) {
-                modalImage.classList.remove('zoomed');
-            }
-        }
-    }
-
-    // 확대 이벤트 감지
-    window.visualViewport.addEventListener('resize', checkZoomLevel);
-    window.visualViewport.addEventListener('scroll', checkZoomLevel);
-
-    // 초기 로딩 시에도 한 번 체크
-    checkZoomLevel();
-
-    // SimpleLightbox 열릴 때 블러 효과 추가
-    lightbox.on('shown.simplelightbox', function() {
-        checkZoomLevel();
-    });
-
-    // SimpleLightbox 닫힐 때 블러 효과 제거
-    lightbox.on('closed.simplelightbox', function() {
-        var modalImage = document.querySelector('.simple-lightbox img');
-        portfolioSection.classList.remove('zoomed');
-        if (modalImage) {
-            modalImage.classList.remove('zoomed');
-        }
     });
 });
